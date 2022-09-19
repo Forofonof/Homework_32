b4a34d007be093f85e1e23b8463378834d6631eb
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-19 15:16:58 +0300

    Добавьте файлы проекта.

---
+++ Homework_32/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        const string exit = "exit";
+        bool isDictionaryActive = true;
+
+        Dictionary<string, string> word = new Dictionary<string, string>();
+        word.Add("Ассимиляция", "Растворение одного народа в другом.");
+        word.Add("Валёжина", "Дерево, упавшее на землю.");
+        word.Add("Дефиниция", "Краткое определение какого-либо понятия.");
+        word.Add("Диссонанс", "Негармоническое сочетание звуков.");
+
+        Console.WriteLine($"Добро пожаловать в словать!\nДля завершения программы, введите: {exit}\nНапишите слово, чтобы узнать его значение:");
+
+        while (isDictionaryActive)
+        {
+            string userInputWord = Console.ReadLine();
+
+            if (word.ContainsKey(userInputWord))
+            {
+                Console.WriteLine($"Определение: {word[userInputWord]}");
+            }
+            else
+            {
+                Console.WriteLine("Нет такого слова в словаре.");
+            }
+            if (userInputWord.ToLower() == exit)
+            {
+                Console.Clear();
+                Console.WriteLine("Работа программы завершена!");
+
+                isDictionaryActive = false;
+            }
+        }
+    }
+}
